6f6f1baa01e162e491aad0151c9d2f912a13b826
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2016-08-25 18:28:49 +0100

    Add users table to the db

---
+++ protected/data/schema.sql
@@ -0,0 +1,13 @@
+/* create users table */
+CREATE TABLE users (
+  username      VARCHAR(128) NOT NULL PRIMARY KEY,
+  email         VARCHAR(128) NOT NULL,
+  password      VARCHAR(128) NOT NULL,  /* in plain text */
+  role          INTEGER NOT NULL,       /* 0: normal user, 1: administrator */
+  first_name    VARCHAR(128),
+  last_name     VARCHAR(128)
+);
+
+/* insert some initial data records for testing */
+INSERT INTO users VALUES ('admin', 'admin@example.com', 'admin', 1, 'Administrator', 'User');
+INSERT INTO users VALUES ('user', 'demo@example.com', 'user', 0, 'Normal', 'User');
