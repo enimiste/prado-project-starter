620267aff38ee36ddc0cc179308e13bba7a81029
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2016-08-25 18:28:34 +0100

    Add LoginPage

---
+++ protected/pages/users/LoginPage.php
@@ -0,0 +1,36 @@
+<?php
+use App\Prado\Page;
+
+/**
+ * Created by PhpStorm.
+ * User: elbachirnouni
+ * Date: 22/08/2016
+ * Time: 13:10
+ */
+class LoginPage extends Page {
+
+	/**
+	 * @param TButton         $sender
+	 * @param TEventParameter $param
+	 */
+	public function signInBtnClicked( $sender, $param ) {
+		if ( page()->getIsValid() ) {
+
+			$url = auth()->getReturnUrl();
+			if ( empty( $url ) ) {
+				$url = page_service()->getDefaultPageUrl();
+			}
+
+			redirect_url( $url );
+		}
+	}
+
+	/**
+	 * @param TCustomValidator              $sender
+	 * @param TServerValidateEventParameter $param
+	 */
+	public function validateUser( $sender, $param ) {
+
+		$param->IsValid = auth()->login( $this->UsernameTxt->Text, $this->PasswordTxt->Text );
+	}
+}
