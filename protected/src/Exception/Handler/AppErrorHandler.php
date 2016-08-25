bec49dcc606de844a9707a9c7c37d3db7e82b72b
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2016-08-25 18:43:13 +0100

    Add Error handler

---
+++ protected/src/Exception/Handler/AppErrorHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Exception\AppException;
+
+\Prado::using( 'System.Exceptions.TErrorHandler' );
+
+/**
+ * Created by PhpStorm.
+ * User: elbachirnouni
+ * Date: 24/08/2016
+ * Time: 16:09
+ */
+class AppErrorHandler extends \TErrorHandler {
+	/**
+	 * @param $statusCode
+	 * @param $exception
+	 *
+	 * @return string
+	 */
+	protected function getErrorTemplate( $statusCode, $exception ) {
+		if ( $exception instanceof AppException ) {
+			return <<<EOF
+			<html><head><title>App error</title></head><body>
+			<div style="color: red;"> %%ErrorMessage%% </div>
+			</body></html>
+EOF;
+		}
+
+		return parent::getErrorTemplate( $statusCode, $exception );
+
+	}
+
+
+	/**
+	 * @param $statusCode
+	 * @param $exception
+	 */
+	protected function handleExternalError( $statusCode, $exception ) {
+		if ( $exception instanceof AppException ) {
+			log( $exception->getErrorMessage(), \TLogger::ERROR, 'AppException' );
+		}
+		parent::handleExternalError( $statusCode, $exception );
+	}
+
+}
